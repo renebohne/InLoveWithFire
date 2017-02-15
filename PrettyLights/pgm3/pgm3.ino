6865af8610f1422caffb35c8a74810d3c76fc84a
René Bohne <rene.bohne@gmail.com>
2017-02-15 14:32:27 +0100

    Pgm3 initial commig
    For pretty lights

---
+++ PrettyLights/pgm3/pgm3.ino
@@ -0,0 +1,241 @@
+// Button hängt zwischen PB1 und PB2,
+// LEDs an PB0.
+
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 0
+
+#define BTN_INPUT 1
+#define BTN_GND 2
+
+volatile int mode = -1;//0 macht alles aus... 1-4 sind die Programme
+
+boolean BACK_TO_MAIN = false;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(51, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAY_PGM1 10
+#define DELAY_PGM2 130
+#define DELAY_PGM3 200
+#define DELAY_FLASH_PGM4 110
+
+
+
+//das sind die Nummern der LEDs von unten nach oben - natürliche Zähleweise von 1 an
+int lednumbers[] = {11, 26, 10, 12, 25, 27, 9, 13, 24, 28, 8, 14, 23, 29, 19, 15, 22, 30, 18, 16, 21, 31, 17, 20, 32, 51, 50, 33, 49, 34, 48, 35, 47, 36, 46, 37, 45, 38, 44, 39, 43, 40, 42, 41};
+
+//das sind die Anzahlen der LEDs pro Layer (eine Reihe)
+int lengths[] = {2, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+//das sind die Anzahlen der LEDs pro Etage für den Regenbogen(mehrere Reihen möglich)
+int length_etagen[] = {10, 12, 6, 4, 4, 4, 3};
+
+void layerByLayer()
+{
+  int lednumbers_idx = 0;
+
+  for (int i = 0; i < (sizeof(lengths) / sizeof(int)); i++)
+  {
+    for (int j = 0; j < lengths[i]; j++)
+    {
+      strip.setPixelColor(lednumbers[lednumbers_idx + j], strip.Color(0, 0, 200));
+    }
+    strip.show();
+    lednumbers_idx += lengths[i];
+  }
+}
+
+int pgm3_leds[] = { 19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,  32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,  20,21,22,23,24,25,26,27,28,29};
+
+
+
+/*
+
+Programm 3:
+
+Farbe:grün/türkis. Hose füllt sich von unten nach oben in 2 parallelen Streifen auf.
+es gehen gleichzeitig an und bleiben an:
+8,11,29,26
+dann dazu:
+19,12,30,25
+dann dazu:
+18,13,31,24
+dann dazu:
+17,14,20,23
+
+Dann zu oberteil,
+32 und 51,
+33 und 50,
+34 und 49,
+36 u 48,
+37 und 47
+38 und 46
+39 und 45
+40 und 44
+41  und 43
+42wird und bleibt rot
+
+
+Programmierung Oberteil bleibt gleich  wie bisher aber  gleiche Farbe wie Hose, also läuft voll von vorn und hinten gleichzeitig nach oben/Schulter und geht dann aus, ausser 42, bleibt rot
+1-2.sec Pause dann 2 mal hintereinander der schnelle Schenkelblitz von programm 2,
+7,6,5,4,3,2,1 und nochmal  7,6,5,4,3,2,1
+2 sec pause
+danach wieder von Angfang an auffüllen ,wenns geht jetzt in einer anderen Farbe:, z.B 1 mal auffüllen in blau dann grün, und wieder türkis
+
+
+*/
+
+
+
+void pgm3()
+{
+  /*
+  //5 Sekunden alles aus
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+  }
+  strip.show();
+
+
+  //5 Sekunden warten
+  for (int i = 0; i < 20; i++)
+  {
+    delay(250);
+    if (BACK_TO_MAIN)
+    {
+      return;
+    }
+  }
+  */
+  
+  int tuerkis = strip.Color(0, 0xDD, 0xDD);  
+  int rot = strip.Color(255, 0, 0);
+
+
+  strip.setPixelColor(8, tuerkis);
+  strip.setPixelColor(11, tuerkis);
+  strip.setPixelColor(29, tuerkis);
+  strip.setPixelColor(26, tuerkis);  
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(19, tuerkis);
+  strip.setPixelColor(12, tuerkis);
+  strip.setPixelColor(30, tuerkis);
+  strip.setPixelColor(25, tuerkis);  
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(18, tuerkis);
+  strip.setPixelColor(13, tuerkis);
+  strip.setPixelColor(31, tuerkis);
+  strip.setPixelColor(24, tuerkis);  
+  strip.show();
+  delay(DELAY_PGM3);
+
+
+  strip.setPixelColor(17, tuerkis);
+  strip.setPixelColor(14, tuerkis);
+  strip.setPixelColor(20, tuerkis);
+  strip.setPixelColor(23, tuerkis);  
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(32, tuerkis);
+  strip.setPixelColor(51, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(33, tuerkis);
+  strip.setPixelColor(50, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+  
+  strip.setPixelColor(34, tuerkis);
+  strip.setPixelColor(49, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(36, tuerkis);
+  strip.setPixelColor(48, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+  
+  strip.setPixelColor(37, tuerkis);
+  strip.setPixelColor(47, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(38, tuerkis);
+  strip.setPixelColor(46, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+  
+  strip.setPixelColor(39, tuerkis);
+  strip.setPixelColor(45, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(40, tuerkis);
+  strip.setPixelColor(44, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+
+  strip.setPixelColor(41, tuerkis);
+  strip.setPixelColor(43, tuerkis);
+  strip.show();
+  delay(DELAY_PGM3);
+  
+  strip.setPixelColor(42, rot);
+  strip.show();
+  delay(DELAY_PGM3);
+  
+}
+
+
+
+void setup()
+{
+  clock_prescale_set(clock_div_1);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+
+
+void loop() {
+    pgm3();
+}
+
+
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void rainbow(int* _leds, uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<sizeof(_leds)/sizeof(int); i++) {
+      strip.setPixelColor(lednumbers[_leds[i]], Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
